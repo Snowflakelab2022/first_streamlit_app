dbd075ac09c9eb1543b11200f2d25a22fb0fc6b2
Snowflakelab2022 <113702649+Snowflakelab2022@users.noreply.github.com>
2022-09-16 10:20:01 -0500

    Update streamlit_app.py
    Added pandas library.
    Added a read line to read fruit macros from S3 bucket
    Added a line to show the list as dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -6,4 +7,7 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
