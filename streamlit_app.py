2b78d15afa0225d9f0149ba42417a1f916a73bb2
Snowflakelab2022 <113702649+Snowflakelab2022@users.noreply.github.com>
2022-09-19 14:40:21 -0500

    Update streamlit_app.py
    Adding an input text to get the fruit from keyboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,12 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruit.
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
@@ -32,3 +37,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+
+
